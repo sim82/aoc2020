e89f1a995979138e0261e9f693e3771a81318b06
sim <sim@arch-vile.fritz.box>
2020-12-06 13:16:18 +0100

    messing around

--- examples/day06.rs
+++ examples/day06.rs
@@ -3,11 +3,11 @@ use std::{collections::HashSet, io::Read};
 
 fn union(group: &str) -> usize {
     group
-        .replace("\n", "")
         .chars()
+        .filter(|c| !c.is_whitespace())
         .collect::<HashSet<_>>()
         .len()
-    // let mut answers: Vec<_> = group.replace("\n", "").chars().collect();
+    // let mut answers: Vec<_> = group.chars().filter(|c| *c != '\n').collect();
     // answers.sort();
     // answers.dedup();
     // answers.len()
@@ -16,8 +16,8 @@ fn union(group: &str) -> usize {
 fn intersection(group: &str) -> usize {
     group
         .split("\n")
-        .map(|person| person.chars().collect::<HashSet<char>>())
-        .fold_first(|a, b| a.intersection(&b).cloned().collect::<HashSet<char>>())
+        .map(|person| person.chars().collect::<HashSet<_>>())
+        .fold_first(|ref a, ref b| a & b)
         .unwrap()
         .len()
 }
@@ -25,6 +25,7 @@ fn intersection(group: &str) -> usize {
 fn main() {
     let mut input = String::new();
     std::io::stdin().lock().read_to_string(&mut input).unwrap();
+    println!("start");
     println!(
         "union: {}",
         input.split("\n\n").map(|g| union(g)).sum::<usize>()
